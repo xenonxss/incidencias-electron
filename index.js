9d8a99aa610fabaa99986408e72e459798684af7
xenon <63518404+xenonxss@users.noreply.github.com>
2022-05-31 09:18:20 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  // and load the index.html of the app.
+  mainWindow.loadFile('index.html')
+
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
---
+++ prok.js
@@ -0,0 +1,67 @@
+function cargarIncidencias() {
+
+    listaIncidencias = document.getElementById('incidencias');
+
+    listaIncidencias.innerHTML = '';
+
+    for (let index = 0; index < localStorage.length; index++) {
+        inc = localStorage.getItem(`inc-${index}`);
+        inc = JSON.parse(inc)[0];
+        listaIncidencias.innerHTML += `
+            <div class="incidencia">
+                ${inc["nombre"]} | ${inc['descripcion']} | Fecha: ${inc['fecha']}
+            </div>
+        `;
+    }
+}
+
+cargarIncidencias();
+
+
+async function generarIncidencia(
+    nombre = null,
+    descripcion = null,
+    ordenAveria = null,
+    recibo = null,
+    presupuesto = 0,
+    factura = null,
+    fecha = null,
+    numeropuesto = null) {
+
+    incidencia = [{
+        nombre: nombre,
+        descripcion: descripcion,
+        ordenAveria: ordenAveria,
+        recibo: recibo,
+        presupuesto: presupuesto,
+        factura: factura,
+        fecha: fecha,
+        numeropuesto: numeropuesto
+    }];
+
+    localStorage.setItem('inc-' + localStorage.length, JSON.stringify(incidencia));
+
+    return console.log('[+]Incidencia Agregada!')
+}
+
+
+$('#subirIncidencia').click(function (event) {
+
+    event.preventDefault();
+
+    let nombre = document.getElementById('nombreIncidencia').value;
+    let descripcion = document.getElementById('descripcionIncidencia').value;
+    let ordenaveria = document.getElementById('ordenaveriaIncidencia').value;
+    let recibo = document.getElementById('reciboIncidencia').value;
+    let presupuesto = document.getElementById('presupuestoIncidencia').value;
+    let factura = document.getElementById('facturaIncidencia').value;
+    let fecha = document.getElementById('fechaIncidencia').value;
+    let numpuesto = document.getElementById('numpuestoIncidencia').value;
+
+    generarIncidencia(
+        nombre, descripcion, ordenaveria, recibo, presupuesto, factura, fecha, numpuesto
+    ).then(() => {
+        cargarIncidencias();
+    })
+
+});
